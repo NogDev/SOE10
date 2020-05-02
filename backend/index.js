bc499f5805db0156b92df72da0b8f06f5a31ac2e
nog <nogueira@pm.ba.gov.br>
2020-05-02 20:14:26 -0300

    Configurando conexão com o mongo

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://root:root@cluster0-jipsb.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
